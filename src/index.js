bf471daaf3977b183368606e4e025df8511168e8
Edward Tanguay (windows) <edward@tanguay.info>
2023-09-11 14:18:13 +0200

    finished

--- src/index.js
+++ src/index.js
@@ -1,27 +1,29 @@
+import fs from 'fs';
 import express from 'express';
 
 const app = express();
 const port = 4201;
 
 app.get('/', (req, res) => {
-	res.send('the backend works');
+	res.send(`
+<h1>MERN APP API</h1>
+
+<ul>
+	<li>/ = main site</li>
+</ul>
+	`);
 });
 
 app.get('/employees', (req, res) => {
-	res.send([
-		{
-			firstName: 'Hans'
-		},
-		{
-			firstName: 'Alice'
-		}
-	]);
+	const jsonEmployees = fs.readFileSync('./src/data/employees.json', 'utf8');
+	const employees = JSON.parse(jsonEmployees);
+	res.send(employees);
 });
 
-app.delete('/employee/:id', (req, res) => {
-	const { id } = req.params;
-	res.send(`Deleting employee #${id}`);
-});
+// app.delete('/employee/:id', (req, res) => {
+// 	const { id } = req.params;
+// 	res.send(`Deleting employee #${id}`);
+// });
 
 app.listen(port, () => {
 	console.log(`listening on port http://localhost:${port}`);
